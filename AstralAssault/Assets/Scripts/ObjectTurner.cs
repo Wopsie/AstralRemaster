1f8120ae76ace778019ea3cdf14dcf9bfb82b4ed
Wopsie <joaghim.beltman@gmail.com>
2016-11-03 16:39:18 +0100

    fixed playermovement rotation

--- AstralAssault/Assets/Scripts/ObjectTurner.cs
+++ AstralAssault/Assets/Scripts/ObjectTurner.cs
@@ -10,26 +10,19 @@ public class ObjectTurner : MonoBehaviour {
     [SerializeField]
     private GameObject plane;
     
-    void FixedUpdate()
-    {
-        //transform.position = plane.transform.position;
-        //transform.eulerAngles = new Vector3(plane.transform.eulerAngles.x, plane.transform.eulerAngles.y, plane.transform.eulerAngles.z);
-
-        //transform.rotation = plane.transform.rotation;
-    }
-
     public void TurnObj(float multiplier)
     {
-        //bankAngle = new Vector3(plane.transform.localRotation.x, plane.transform.localRotation.y, plane.transform.localRotation.z + 25);
-        bankAngle = new Vector3(this.transform.localRotation.x, this.transform.localRotation.y, this.transform.localRotation.z + 25);
+        bankAngle = new Vector3(plane.transform.localRotation.x, plane.transform.localRotation.y, plane.transform.localRotation.z + 25);
 
         if(multiplier != 0)
         {
+            //turn object according to analogue input multiplier
             transform.localEulerAngles = bankAngle * -multiplier;
         }
         else
         {
-            transform.localEulerAngles = new Vector3(plane.transform.rotation.x, plane.transform.rotation.y, plane.transform.rotation.z);
+            //assume rotation of the parent 
+            transform.rotation = plane.transform.rotation;
         }
     }
 }
--- AstralAssault/Assets/Scripts/ShipMovement.cs
+++ AstralAssault/Assets/Scripts/ShipMovement.cs
@@ -14,25 +14,8 @@ public class ShipMovement : MonoBehaviour {
     [SerializeField]
     private KeyboardInput keyboard;
 
-    [SerializeField]
-    private GameObject bankRefrence;
-    [SerializeField]
-    private ObjectTurner bankRefrenceScript;
-
-    [SerializeField]    //this gameobject is the child of the gameobject this class is attached to that should be banking
-    private GameObject hull;
-
-    //steadily increase euleranglevelocity to make smooth rotation. 
-    private Vector3 eulerAngleVelocity = Vector3.zero;
-
-    private Vector3 turningVector = Vector3.zero;
-
-    private int bankingBuffer;
-
     public float torque;
-
-    //replace this with some vector to make Z axis rotation smooth
-    public Transform target;
+    public ObjectTurner objTurnScript;
 
     void Start()
     {
@@ -41,92 +24,30 @@ public class ShipMovement : MonoBehaviour {
 
     void FixedUpdate()
     {
-        ShipAccelleration();
         TurningInput();
-
-        
-
-        /*
-        Quaternion current = transform.localRotation;
-        Quaternion rotation = Quaternion.LookRotation(target.position - transform.position);
-        transform.localRotation = Quaternion.Slerp(current, rotation, Time.deltaTime);
-        */
-    }
-
-    void BankingInput()
-    {
-        if (keyboard.e)
-        {
-            //bank right
-            eulerAngleVelocity.z += 5;
-
-            ShipBanking(-1);
-            bankingBuffer = -1;
-        }
-        else if (keyboard.q)
-        {
-            //bank left
-            eulerAngleVelocity.z += 5;
-
-            ShipBanking(1);
-            bankingBuffer = 1;
-        }
-
-        //slow down rotation
-        if (!keyboard.e && !keyboard.q)
-        {
-            if (eulerAngleVelocity.z > 0)
-            {
-                eulerAngleVelocity.z -= 5;
-                ShipBanking(bankingBuffer);
-            }
-        }
+        ShipAccelleration();
     }
 
     void TurningInput()
     {
-        //convert vector 3 to quaternion
 
+        Debug.Log(transform.up);
         //turn ship
         float turnHorizontal = Input.GetAxis("Horizontal") * 2f;
-        rb.AddTorque(transform.up * torque * turnHorizontal);
+        rb.AddTorque(new Vector3(0,1,0) * torque * turnHorizontal);
 
         //up & down
         float turnVertical = Input.GetAxis("Vertical") * 3.5f;
         rb.AddTorque(transform.right * torque * turnVertical);
 
-        bankRefrenceScript.TurnObj(turnHorizontal);
 
-        //Debug.Log(rb)
-        //hull.transform.localEulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, transform.localEulerAngles.z);
-        //rb.rotation = Quaternion.Slerp(hull.transform.localRotation, bankRefrence.transform.rotation, Time.time * 0.05f);
-
-        hull.transform.rotation = Quaternion.Slerp(hull.transform.localRotation, bankRefrence.transform.localRotation, 2 * 0.05f);
+        objTurnScript.TurnObj(turnHorizontal);
     }
 
     //speed up and slow down ship
     void ShipAccelleration()
     {
         //transform.position += transform.forward * Time.deltaTime * 30;
-        rb.AddRelativeForce(new Vector3(0,0,900) * Time.deltaTime * 10);
-    }
-
-    //ships sideways rotation
-    void ShipBanking(int negative)
-    {
-        //aligns this rotation with rotation of that of a target transform.     check if this can be used to make a banking effect
-        
-
-
-
-        if (eulerAngleVelocity.z > 100)
-        {
-            eulerAngleVelocity.z = 100;
-        }
-
-        eulerAngleVelocity = eulerAngleVelocity * negative;
-        Quaternion deltaRotation = Quaternion.Euler(eulerAngleVelocity * Time.deltaTime);
-        rb.MoveRotation(rb.rotation * deltaRotation);
-        eulerAngleVelocity = eulerAngleVelocity * negative;
+        rb.AddRelativeForce(new Vector3(0,0,900) * 0.5f);
     }
 }
