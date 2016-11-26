943b6f9c1ae1ed27f72e91c9fc21a99dcf5ddb93
Wopsie <joaghim.beltman@gmail.com>
2016-11-27 00:35:03 +0100

    added simple UI element for enemy.

---
+++ AstralAssault/Assets/Scripts/ZeBull/ExampleClass.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ExampleClass : MonoBehaviour {
+
+    public Transform target;
+    Camera camera;
+
+    public Image lockonImage;
+
+    void Start()
+    {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        Vector3 screenPos = camera.WorldToScreenPoint(target.position);
+        //Debug.Log("target is " + screenPos.x + " pixels from the left");
+
+        Debug.Log(screenPos);
+
+        if(screenPos.x >= 0 && screenPos.x < Screen.width)
+        {
+            lockonImage.rectTransform.position = screenPos;
+        }
+        else
+        {
+            lockonImage.rectTransform.position = Vector3.one;
+        }
+        
+    }
+}
